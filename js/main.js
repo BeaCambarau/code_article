e07a238a79bb5f90214ae116d8ba7dd1c341ad54
Beatrice <Beatrice@MacBook-Pro-di-Beatrice.local>
2016-06-14 16:51:20 +0200

    download page

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
                 , 30, 406, 620);
    context.fillStyle = '#282828';
    context.fill();
+   
    var canvas = document.getElementById('Canvas');
    var context = canvas.getContext('2d');
 
