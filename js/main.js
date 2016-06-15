5a86dbf44ebfd62e5486d66ffe005387235906d6
Beatrice <Beatrice@MacBook-Pro-di-Beatrice.local>
2016-06-15 10:30:24 +0200

    piccoli assetti

--- js/main.js
+++ js/main.js
@@ -3,20 +3,26 @@ $(document).ready(function(){
      $('.parallax').parallax();
    });
 
+   var canvas = document.getElementById('c3');
+   var context = canvas.getContext('2d');
+
+   context.beginPath();
+   context.rect(90, 30, 406, 620);
+   context.fillStyle = '#282828';
+   context.fill();
+
    var canvas = document.getElementById('h5');
    var context = canvas.getContext('2d');
 
    context.beginPath();
-   context.rect(90
-                , 30, 406, 620);
-   context.fillStyle = '#282828';
+   context.rect(90, 10, 400, 230);
+   context.fillStyle = '#2C314D';
    context.fill();
-   
-   var canvas = document.getElementById('Canvas');
+
+   var canvas = document.getElementById('js');
    var context = canvas.getContext('2d');
 
    context.beginPath();
-   context.rect(90
-                , -10, 400, 230);
-   context.fillStyle = '#282828';
+   context.rect(90, 10, 400, 230);
+   context.fillStyle = '#2C314D';
    context.fill();
