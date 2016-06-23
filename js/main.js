beb76542c31235d797e1fb9b9636aba173167094
Antonio Molinari <molinari@incode.it>
2016-06-23 16:38:56 +0200

    molinari :-)

--- js/main.js
+++ js/main.js
@@ -2,14 +2,44 @@
 
 $(document).ready(function() {
 
-  $('#fullpage').fullpage({
-    anchors: ['firstPage', 'secondPage', '3rdPage'],
-    //sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
-    css3: true
-  });
+  if(true){
+    $('#fullpage').fullpage({
+      anchors: ['firstPage', 'secondPage', '3rdPage'],
+      //sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C'],
+      css3: true
+    });
+  }
+
 
   $(function () {
     $('[data-toggle="popover"]').popover()
   })
+
+  $(window).on("resize", function(event){
+    setupContents();
+  })
+  function setupContents(){
+    $(".fp-tableCell > .content ").each(function(index, element){
+      var content = $(this);
+      //content.hide();
+      var parent = content.parent();
+      var height = parent.height()-$("#header").height()-$("#footer").height()-100;
+      content.height(height);
+      content.find("img").each(function(index,element){
+        $(this).css({
+          "height": content.height(),
+          "width": "auto"
+        })
+        if($(this).width() > content.width()){
+          $(this).css({
+            "width": content.width(),
+            "height": "auto"
+          })
+        }
+      })
+      console.log(parent.height())
+    })
+  }
+  setupContents();
 
 });
